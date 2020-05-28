853e86fb51bcf147dc426c0801ee020f0ca925cb
La Quốc Thắng <quocthang0507@gmail.com>
2020-05-28 14:13:07 +0700

    Add new project to solution

---
+++ 1_BasicSelenium/1_SetupSelenium/Program.cs
@@ -0,0 +1,28 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Text;
+
+namespace _1_SetupSelenium
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.OutputEncoding = Encoding.UTF8;
+			IWebDriver driver = new ChromeDriver(@"D:\OneDrive\Thang\HOCTAP\PUBLIC PROJECTS\SeleniumCSharp\");
+			driver.Url = "http://online.dlu.edu.vn"; //This method Load a new web page in the current browser window.
+			string title = driver.Title;
+			int titleLen = title.Length; // driver.Title.Length;
+			Console.WriteLine($"Title of this page: {title}, length: {titleLen}");
+			string pageUrl = driver.Url;
+			int urlLen = driver.Url.Length;
+			Console.WriteLine($"URL of this page: {pageUrl}, length: {urlLen}");
+			string pageSource = driver.PageSource;
+			int sourceLen = driver.PageSource.Length;
+			Console.WriteLine($"Page length: {sourceLen}");
+			Console.ReadKey();
+			driver.Close();
+		}
+	}
+}
---
+++ 1_BasicSelenium/2_PressButton/Program.cs
@@ -0,0 +1,22 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_PressButton
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			IWebDriver driver = new ChromeDriver(@"D:\OneDrive\Thang\HOCTAP\PUBLIC PROJECTS\SeleniumCSharp");
+			driver.Url = "http://online.dlu.edu.vn/Login";
+			driver.FindElement(By.XPath(".//*[@class='btn btn-primary btn-block']")).Click();
+			Console.ReadKey();
+			driver.Close();
+		}
+	}
+}
