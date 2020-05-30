f019e0549183b6757ac5c8b22b312838f21bb089
La Quốc Thắng <quocthang0507@gmail.com>
2020-05-30 21:02:16 +0700

    Up cho có lệ thôi chứ như cái project trước rồi 😂

---
+++ 4_FindElements/4_FindElements/Program.cs
@@ -0,0 +1,27 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_FindElements
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			IWebDriver driver = new ChromeDriver(@"D:\OneDrive\Thang\HOCTAP\PUBLIC PROJECTS\SeleniumCSharp");
+			driver.Url = "https://shopee.vn/";
+			var btnClosePopup = driver.FindElement(By.ClassName("shopee-popup__close-btn"));
+			btnClosePopup.Click();
+			var tbxSearch = driver.FindElement(By.ClassName("shopee-searchbar-input__input"));
+			tbxSearch.SendKeys("luong kho");
+			tbxSearch.SendKeys(Keys.Enter);
+			Console.ReadKey();
+			driver.Close();
+
+		}
+	}
+}
