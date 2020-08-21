cb1fe7292926b32d56f0aa7619cd646dc8ee1ab6
La Quốc Thắng <quocthang0507@gmail.com>
2020-08-21 15:56:48 +0700

    Upload new project

---
+++ ChromeWithExistingProfile/ChromeWithExistingProfile/Program.cs
@@ -0,0 +1,23 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChromeWithExistingProfile
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			ChromeOptions options = new ChromeOptions();
+			options.AddArguments(@"--user-data-dir=C:\Users\quoct\AppData\Local\Google\Chrome\User Data\Default");
+			IWebDriver driver = new ChromeDriver(@"D:\OneDrive\Thang\HOCTAP\PUBLIC PROJECTS\SeleniumCSharp", options);
+			driver.Navigate().GoToUrl("https://www.google.com.vn/?gws_rd=ssl");
+			Console.ReadKey();
+			driver.Close();
+		}
+	}
+}
